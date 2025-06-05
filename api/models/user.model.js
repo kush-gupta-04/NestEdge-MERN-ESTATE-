9cf6cd2ec30ccc0ac8e573fe8bc40bc2628ba08e
Kushagra Gupta <kushagragupta0401@gmail.com>
2025-06-05 14:54:57 +0530

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose, { model } from "mongoose";
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const user = mongoose.model('User', userSchema);
+
+export default user;
